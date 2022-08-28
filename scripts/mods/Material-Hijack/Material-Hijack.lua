317f0b51fc09e94a7b5b03d8c3514a79e6da4650
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-08-27 23:01:53 -0500

    update ignore, remove comments, remove extraneous files

--- scripts/mods/Material-Hijack/Material-Hijack.lua
+++ scripts/mods/Material-Hijack/Material-Hijack.lua
@@ -14,20 +14,6 @@ local dict = { --this table is just for debugging
     texture_map_4617b8e0 = "textures/default_emis",
 } 
 
---this function is depricated
-local function check_unit_slots(material, slot_name, texture_path, unit)
-    if slot_name == nil or texture_path == nil then 
-        -- mod:error("Slot "..tostring(slot_name).." not found in "..tostring(unit).." or texture path is "..tostring(texture_path)) 
-        local j = nil --for personal debug printing
-    -- elseif texture_path == nil then
-    --     print("|    |"..slot_name.."|    |"..tostring(dict[slot_name]))
-    --     Material.set_texture(material, slot_name, dict[slot_name])
-    else
-        -- print("|    |"..slot_name.."|    |"..texture_path)
-        Material.set_texture(material, slot_name, texture_path)
-    end
-end
-
 local function replace_textures(unit)
     if Unit.has_data(unit, "mat_to_use") then
         local mat_slots = {}
@@ -63,13 +49,10 @@ local function replace_textures(unit)
 
             
             dict[mat_slots[i]] = {}
-            -- if Unit.has_data(unit, "mat_slots", "slot"..tostring(i)) then 
-            --     mat_slots[i] = Unit.get_data(unit, "mat_slots", "slot"..tostring(i))
-            -- end
+
             if Unit.has_data(unit, "colors", "slot"..tostring(i)) then 
                 colors[i] = Unit.get_data(unit, "colors", "slot"..tostring(i))
                 dict[mat_slots[i]]['color_slot'] = Unit.get_data(unit, "colors", "slot"..tostring(i))
-                -- mod:echo("slot"..tostring(i)..":    "..tostring(colors[i]))
             else
                 dict[mat_slots[i]]['color_slot'] = colors[i]
             end
@@ -95,39 +78,9 @@ local function replace_textures(unit)
             end
         end
 
-
-
-        -- local color_slot = Unit.get_data(unit, "color_slot")
-        -- local norm_slot = Unit.get_data(unit, "norm_slot")
-        -- local MAB_slot = Unit.get_data(unit, "MAB_slot")
-        -- local emis_col_slot = Unit.get_data(unit, "emis_col_slot")
-        -- local emis_det_slot = Unit.get_data(unit, "emis_det_slot")
-        -- local num_meshes = Unit.num_meshes(unit)
-        -- for index, mat_slot in pairs(mat_slots) do 
-        --     Unit.set_material(unit, mat_slot, mat)
-        --     -- print(mat_slot)
-        --     -- print(index)
-        --     for i = 0, num_meshes - 1, 1 do
-        --         local mesh = Unit.mesh(unit, i)
-        --         local num_mats = Mesh.num_materials(mesh)
-        --         for j = 0, num_mats - 1, 1 do
-        --             local mat = Mesh.material(mesh, j)
-        --             -- print(mat)
-        --             check_unit_slots(mat, color_slot, colors[j+1], unit)
-        --             mod:echo(tostring(mat_slot)..":   "..tostring(colors[j+1]))
-        --             check_unit_slots(mat, norm_slot, normals[j+1], unit)
-        --             check_unit_slots(mat, MAB_slot, MABs[j+1], unit)
-        --             check_unit_slots(mat, emis_col_slot, emis_colors[j+1], unit)
-        --             check_unit_slots(mat, emis_det_slot, emis_details[j+1], unit)
-        --         end
-        --     end
-        -- end
-
-
         for mat_slot, texture in pairs(dict) do 
             Unit.set_material(unit, mat_slot, mat)
             local num_meshes = Unit.num_meshes(unit)
-            -- mod:echo(tostring(mat_slot).." : "..tostring(texture))
             for i = 0, num_meshes - 1, 1 do
                 local mesh = Unit.mesh(unit, i)
                 local num_mats = Mesh.num_materials(mesh)
@@ -135,9 +88,7 @@ local function replace_textures(unit)
                     if Mesh.has_material(mesh, mat_slot) then
                         local mater = Mesh.material(mesh, mat_slot)
                         for text_slot, map in pairs(texture) do
-                            -- mod:echo(tostring(text_slot)..":     "..tostring(map))
-                            Material.set_texture(mater, Unit.get_data(unit, text_slot), map)                            
-                            -- mod:echo(tostring(unit).." : "..tostring(text_slot).." : "..tostring(map))
+                            Material.set_texture(mater, Unit.get_data(unit, text_slot), map)
                         end
                     end
                 end
@@ -151,9 +102,6 @@ local function replace_textures(unit)
         for i=1, num_mats do 
             local mat_slot = Unit.get_data(unit, "mat_slots", "slot"..tostring(i))
             local mat = Unit.get_data(unit, "mat_list", "slot"..tostring(i))
-            -- print(mat)
-            -- print(mat_slot)
-            -- Managers.package:load(mat, "global")
             Unit.set_material(unit, mat_slot, mat)
         end
     end
@@ -166,17 +114,6 @@ function(func, world, slot_name, item_data, unit_1p, unit_3p, is_bot, unit_templ
     replace_textures(unit_3p)
     return func(world, slot_name, item_data, unit_1p, unit_3p, is_bot, unit_template, extra_extension_data, ammo_percent, override_item_template, override_item_units)
 end)
-
--- mod:hook(UnitSpawner, 'create_unit_extensions', function (func, self, world, unit, unit_template_name, extension_init_data)
---     replace_textures(unit)
---     -- local bones = Unit.bones(unit)
---     -- if bones then
---     --     for k,v in pairs(bones) do 
---     --         print('"'..tostring(v)..'",')
---     --     end
---     -- end
---     return func(self, world, unit, unit_template_name, extension_init_data)
--- end)
 
 mod:hook(UnitSpawner, 'spawn_local_unit', function (func, self, unit_name, position, rotation, material)
     local unit = World.spawn_unit(self.world, unit_name, position, rotation, material)
@@ -196,11 +133,6 @@ mod:hook(HeroPreviewer, '_spawn_item_unit', function (func, self, unit, item_slo
     replace_textures(unit)
     return func(self, unit, item_slot_type, item_template, unit_attachment_node_linking, scene_graph_links, material_settings)
 end)
-
--- mod:hook(ScriptUnit,'extension', function (func, unit, system_name)
---     replace_textures(unit)
---     return func(unit, system_name)
--- end)
 
 local function spawn_package_to_player (package_name)
 	local player = Managers.player:local_player()
